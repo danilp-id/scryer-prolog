2dc87376d44d6763305aadeb135a8c417cb4ccea
Danil Platonov <platonov.danil.ivanovich@gmail.com>
2026-08-02 05:16:37 -0700

    typo fix

--- src/lib/http/http_server.pl
+++ src/lib/http/http_server.pl
@@ -124,7 +124,7 @@ http_listen(Port, Module:Handlers0) :-
 % - `tls_cert(+Cert)` - a TLS cert for HTTPS (string)
 % - `content_length_limit(+Limit)` - maximum length (in bytes) for the incoming bodies. By default, 32KB.
 % - `initial_state(+State)` - initial server state. By default, `[]`. Can use any term as initial state.
-% - `catch_errors(+Boolean)` - whether to catch all program errors (aside for interrupts). By default, web server will exit if any programming error is encountered.
+% - `catch_errors(+Boolean)` - whether to catch all program errors (aside from interrupts). By default, web server will exit if any programming error is encountered.
 %
 % In order to have a HTTPS server (instead of plain HTTP), both `tls_key` and `tls_cert` options must be provided.
 http_listen(Port, Module:Handlers0, Options) :-
